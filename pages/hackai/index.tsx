d53e206de873549d32f8705351f906e84579ffb6
Sreevasan Siva <ssiva@Sreevasans-MacBook-Pro.local>
2025-11-24 20:53:32 -0600

    Added the hackai route

--- pages/hackai/index.tsx
+++ pages/hackai/index.tsx
@@ -4,7 +4,7 @@ export default function HackAiHome() {
   return (
     <div className="pt-[5rem]">
       {/* Your other page content */}
-
+      
       <PastWinners />
 
       {/* More content */}
